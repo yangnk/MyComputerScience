4634eac9b3208c77110b36bfc9beed86de27cb68
yangnk <yangningkai@qq.com>
2018-11-26 21:45:41 +0800

    学习工厂模式

---
+++ src/main/java/designPattaren/factory/methodFactory/CarFactory.java
@@ -0,0 +1,10 @@
+package designPattaren.factory.methodFactory;
+
+/**
+ * Created by yangnk23900 on 2018/11/26.
+ */
+public abstract class CarFactory {
+	abstract void run();
+
+	public abstract Car newInstance();
+}
